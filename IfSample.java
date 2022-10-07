357bbeba1e41f80a039035ef627ab44f21c3f865
21BQ1A4252 <113115757+21BQ1A4252@users.noreply.github.com>
2022-10-07 07:18:28 +0530

    Add files via upload

---
+++ IfSample.java
@@ -0,0 +1,12 @@
+class IfSample{
+public static void main(String[] args){
+int x,y;
+x=10;
+y=20;
+if(x<y) System.out.println("x is less than y");
+x*=2;
+if(x==y) System.out.println("xis equal to y");
+x*=2;
+if(x>y) System.out.println("x is greater than y");
+}
+} 
